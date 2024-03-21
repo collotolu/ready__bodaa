0192ccb07242e488722f5f6515ff73f56b5832d4
collo <otienomito99@gmail.com>
2024-03-21 18:25:00 +0300

    changed the apply section

--- js/Apply.js
+++ js/Apply.js
@@ -1,4 +1,16 @@
-document.getElementById("loanForm").addEventListener("click", (event) => {
+const form = document.querySelector("#inputAppllication");
+console.log(form);
+
+const messages = document.querySelector(".message");
+console.log(messages);
+
+const inputItems = document.querySelectorAll("input");
+console.log(inputItems);
+
+const pressButton = document.getElementById("btn");
+console.log(pressButton);
+
+document.getElementById("loanForm").addEventListener("submit", (event) => {
   event.preventDefault();
 
   let price = parseFloat(document.getElementById("price").value);
@@ -19,65 +31,35 @@ document.getElementById("loanForm").addEventListener("click", (event) => {
     "<p>Monthly payment: " + monthlyPayment.toFixed(2) + " ksh</p>";
 });
 
-document
-  .getElementById("inputAppllication")
-  .addEventListener("click", (event) => {
-    event.preventDefault();
+pressButton.addEventListener("click", (event) => {
+  event.preventDefault();
 
-    // var firstName = document.getElementById("input").value;
-    // var lastName = document.getElementById("input").value;
-    // var nationalId = document.getElementById("input").value;
-    // var phoneNumber = document.getElementById("input").value;
-
- 
-    const user = {};
-    let users = [];
-    let allValidated = false;
-    inputItems.forEach((input) => {
-      if (input.value === "" || input.value === null) {
-        messages.textContent = "Fill all the empty Fields";
-        input.style.border = "1px solid red";
-      }else if(localStorage.getItem("users") !== null){
-        JSON.parse(localStorage.getItem("users")).forEach(el=>{
-            if(el["national id"] === input.value){
-                messages.textContent = "Identifacation Number Already Exists";
-            }
-        })
-    } else if(localStorage.getItem("users") !== null){
-      JSON.parse(localStorage.getItem("users")).forEach(el=>{
-          if(el["number"] === input.value){
-              messages.textContent = "Phone Number Already Exists";
-          }
-      })
-  } 
-      
-      else {
-        input.style.border = "1px solid black";
-        messages.textContent = "";
-      }
-    });
-
+  let allValidated = false;
+  inputItems.forEach((input) => {
+    if (input.value === "" || input.value === null) {
+      messages.textContent = "Fill all the empty Fields";
+      input.style.border = "1px solid red";
+    } else {
+      input.style.border = "1px solid black";
+      messages.textContent = "";
+    }
+  });
 
   if (messages.textContent === "") {
     allValidated = true;
-    window.location.href = "http://127.0.0.1:5500/index.html";
   }
 
-  //   Local Storage
-  if (allValidated === true) {
-    if (localStorage.getItem("users") === null) {
-      users.push(user);
-      localStorage.setItem("users", JSON.stringify(users));
-    } else {
-      users = JSON.parse(localStorage.getItem("users"));
-      users.push("user");
-
-      localStorage.setItem("users", JSON.stringify(users));
-    }
-  }
+  emailjs.init({
+    publicKey: "mZoERcoOGLkzLLKfo",
   });
-  const messages = document.querySelector(".message");
-  console.log(messages);
+  
+  emailjs.sendForm("service_byc8raf", "contact form", form).then(
+    () => {
+      console.log("SUCCESS!");
+    },
+    (error) => {
+      console.log("FAILED...", error);
+    }
+  );
+});
 
-  const inputItems = document.querySelectorAll("input");
-  console.log(inputItems);
